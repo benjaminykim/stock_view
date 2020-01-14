3f3047bc214736c219b4ab6f48a52a00529390a4
Benjamin Kim <benjaminykim@berkeley.edu>
2020-01-14 13:04:40 -0800

    Updates

---
+++ precache-manifest.9322dd30ea7fb46e94b57174f8bbcf7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da1d9bf14b438ecbe397d1f931ff3c4",
+    "url": "/stock_view/index.html"
+  },
+  {
+    "revision": "6a549a3f60d37d1e44ae",
+    "url": "/stock_view/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f4a77c784a79fd989e2d",
+    "url": "/stock_view/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a549a3f60d37d1e44ae",
+    "url": "/stock_view/static/js/2.bccb779a.chunk.js"
+  },
+  {
+    "revision": "7a5fa0730fa33f7a19d0f8cc77190782",
+    "url": "/stock_view/static/js/2.bccb779a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4a77c784a79fd989e2d",
+    "url": "/stock_view/static/js/main.bd8119b2.chunk.js"
+  },
+  {
+    "revision": "e51a64903687fdbcb437",
+    "url": "/stock_view/static/js/runtime-main.7831caa7.js"
+  }
+]);
