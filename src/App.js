968322a37ebfd6e3faaff7d68b64be6a636638de
Benjamin Kim <benjaminykim@berkeley.edu>
2020-01-14 22:33:44 -0800

    add StockView route paths

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ class App extends React.Component {
         <Router>
         <Container fluid="true">
           <Navbar bg="light" expand="lg" sticky="top">
-            <Navbar.Brand href="/">stock_view</Navbar.Brand>
+            <Navbar.Brand href="/stock_view">stock_view</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/watchlist">Watchlist</Nav.Link>
-                <Nav.Link href="/markets">Markets</Nav.Link>
+                <Nav.Link href="/stock_view">Home</Nav.Link>
+                <Nav.Link href="/stock_view/watchlist">Watchlist</Nav.Link>
+                <Nav.Link href="/stock_view/markets">Markets</Nav.Link>
               </Nav>
               <Form inline>
                 <FormControl type="text" placeholder="AAPL" className="mr-sm-2" />
@@ -30,9 +30,9 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
           <br/>
-          <Route path="/" exact component={StockView} />
-          <Route path="/watchlist" component={WatchlistView} />
-          <Route path="/markets" component={MarketView} />
+          <Route path="/stock_view" exact component={StockView} />
+          <Route path="/stock_view/watchlist" component={WatchlistView} />
+          <Route path="/stock_view/markets" component={MarketView} />
         </Container>
       </Router>
       </div>
