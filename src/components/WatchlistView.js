95804e92fc2ac43ae9a6368bfef494a3d70cfc63
Benjamin Kim <benjaminykim@berkeley.edu>
2020-01-16 13:16:34 -0800

    remove unused imports

--- src/components/WatchlistView.js
+++ src/components/WatchlistView.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import HighChart from './HighChart'
-import StockCard from './StockCard'
-import StockMenu from './StockMenu'
 
 export default class WatchlistView extends React.Component {
   render() {
